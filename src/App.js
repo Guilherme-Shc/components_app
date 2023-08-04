49d6930219e133a665558f70fed9ad4259b3cdf4
Guilherme-Shc <antguischcor@gmail.com>
2023-08-04 01:34:40 -0300

    atualizando o texto dentro do acordeon pra ficar mais bonitinho

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Accordion from "./components/Accordion";
 function App(){
     const items = [
         {
-            id:"1",
-            label: "HIHIHIHI",
-            content: "AUSDHAUSDHAUSHDUASHDAUSDHLASHDLASHDUASHDAUSHDLASHDUI"
+            id:"dont",
+            label: "Hollow Knight",
+            content: "Hollow Knight é uma aventura de ação clássica em estilo 2D por um vasto mundo interligado. Desenvolvido pela Team Cherry e inicialmente lançado para Microsoft Windows em 24 de Fevereiro de 2017, e posteriormente macOS e Linux. Foi portado em 2018 para o Nintendo Switch, Xbox One e Playstation 4 pela Shark Jump Studios."
         },
         {
-            id:"2",
-            label: "HIHIHIHI",
-            content: "AUSDHAUSDHAUSHDUASHDAUSDHLASHDLASHDUASHDAUSHDLASHDUI"
+            id:"mind",
+            label: "The Last of Us",
+            content: "The Last of Us é uma franquia de jogos eletrônicos de ação-aventura e survival horror exclusiva da PlayStation, criada por Neil Druckmann. A franquia é situada em um mundo pós-apocalíptico, com seres humanos hostis e criaturas canibais infectadas por uma mutação do fungo cordyceps."
         },
         {
-            id:"3",
-            label: "HIHIHIHI",
-            content: "AUSDHAUSDHAUSHDUASHDAUSDHLASHDLASHDUASHDAUSHDLASHDUI"
+            id:"me",
+            label: "Persona 5",
+            content: "Persona 5 é um jogo eletrônico de RPG desenvolvido pela Atlus. O jogo é cronologicamente a sexta edição da série Persona, que faz parte principalmente da franquia Megami Tensei."
         },
     ]
     return(<Accordion items={items}/>);
