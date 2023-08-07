10a60ea0049e88078a25851fb2a9519ec242e557
Guilherme-Shc <antguischcor@gmail.com>
2023-08-07 20:00:40 -0300

    made most part of the base of the table component, need to add sorting later

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,33 @@
-function Table() {
+function Table({data, config, keyFn}) {
+
+    const renderedHeaders = config.map((column) =>{
+        return <th key={column.label}>{column.label}</th>
+    });
+
+    const renderedRows = data.map((rowData) =>{
+        const renderedCells = config.map((column) => {
+            return(
+                <td key={column.label} className="p-2">{column.render(rowData)}</td>
+            );
+        });
+        return(
+            <tr className="border-b"key={keyFn(rowData)}>
+                {renderedCells}
+            </tr>
+        )
+    });
+
     return(
-        <div>
-            HI
-        </div>
+        <table className="table-auto border-spacing-2">
+            <thead>
+                <tr key="" className="border-b-2">
+                    {renderedHeaders}
+                </tr>
+            </thead>
+            <tbody>
+                {renderedRows}
+            </tbody>
+        </table>
     );
 }
 
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,27 @@
 import Table from "../components/Table";
 
 function TablePage() {
+    const data =[
+        {name: 'Peach', color:'bg-rose-300/50 border-rose-300 border-2', score: 4},
+        {name: 'Apple', color:'bg-red-500/50 border-red-500 border-2', score: 3},
+        {name: 'Banana', color:'bg-yellow-400/50 border-yellow-400 border-2', score: 2},
+        {name: 'Lemon', color:'bg-green-500/50 border-green-500 border-2', score: 5},
+        {name: 'Blueberry', color:'bg-blue-500/50 border-blue-500 border-2', score: 1},
+    ];
+
+    const config =[
+        { label: 'Name', render: (fruit) => fruit.name},
+        { label: 'Color', render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />},
+        { label: 'Score', render: (fruit) => fruit.score/*, sort: (a, b) => code to implement sortirng*/}
+
+    ]
+
+    const keyFn = (fruit) =>{
+        return fruit.name
+    };
     return(
         <div>
-            <Table />
+            <Table data={data} config={config} keyFn={keyFn}/>
         </div>
     );
 }
